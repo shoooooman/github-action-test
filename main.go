0ba7863f175aba906b11550768f1029f358dfa94
shoooooman <shoma216@gmail.com>
2020-09-05 19:11:57 +0900

    add test file

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func f() {
+	n := 0
+	switch n {
+	case 0:
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+	case 10:
+	}
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
