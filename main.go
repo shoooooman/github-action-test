ad227b121ade96d612b908cc6eb587a312dca76d
shoooooman <shoma216@gmail.com>
2020-09-06 15:02:37 +0900

    fix test cases

--- main.go
+++ main.go
@@ -3,6 +3,35 @@ package main
 import "fmt"
 
 func f1() {
+	n := 0
+	switch n {
+	case 0:
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+	}
+}
+
+func g() {
+	n := 0
+	switch n {
+	case 0:
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+	}
 }
 
 func main() {
