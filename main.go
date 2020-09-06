d686b82aff428a3ecaee652abd8be8160d91fc5c
shoooooman <shoma216@gmail.com>
2020-09-06 13:05:31 +0900

    add test cases

--- main.go
+++ main.go
@@ -3,6 +3,19 @@ package main
 import "fmt"
 
 func f1() {
+	n := 0
+	switch n {
+	case 0:
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+	}
 }
 
 func main() {
