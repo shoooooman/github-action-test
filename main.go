3ec93d24cd10e4ba4c8900a8c230722b0b3e24d5
shoooooman <shoma216@gmail.com>
2020-09-07 00:08:00 +0900

    add test case

--- main.go
+++ main.go
@@ -10,10 +10,29 @@ func f1() {
 	} else if false {
 	} else if false {
 	} else if false {
+	} else if false {
+	} else if false {
+	} else if false {
 	} else {
 	}
 }
 
 func main() {
 	fmt.Println("vim-go")
+}
+
+func f2() {
+	n := 0
+	switch n {
+	case 0:
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+	}
 }
