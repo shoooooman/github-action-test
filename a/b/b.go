278e73a10e2db17fae7a79b704f74a467e2f83f3
shoooooman <shoma216@gmail.com>
2020-09-06 14:05:22 +0900

    add test case

---
+++ a/b/b.go
@@ -0,0 +1,17 @@
+package b
+
+func f3() {
+	if false {
+	} else if false {
+	} else if false {
+	} else if false {
+	} else if false {
+	} else if false {
+	} else if false {
+	} else if false {
+	} else if false {
+	} else if false {
+	} else if false {
+	} else {
+	}
+}
